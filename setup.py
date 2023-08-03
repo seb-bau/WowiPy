12b5f8eabc9b43a1b2f5b037ff968010c0a0b979
bauhaus <s.bauhaus@wbg-luenen.de>
2023-08-03 11:21:29 +0200

    Release v1.1.7 - Ticket

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wowipy',
-    version='1.1.6',
+    version='1.1.7',
     description='OPENWOWI Wowiport API wrapper for python',
     url='https://github.com/seb-bau/WowiPy',
     author='Sebastian Bauhaus',
